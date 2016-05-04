345c3d5e3c009a70c2f7d8017964cf0250bb7c38
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-05-04 06:57:36 +0200

    Updated spec to use factorygirl

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.feature "Users can view projects" do
+  
   scenario "with the project details" do
     project = FactoryGirl.create(:project, name: "Sublime Text 3")
     visit "/"
