8bb3d627a92fb4cc10c314e9c0fd691f37e53fde
Andre Heijstek <andre.heijstek@improvementfocus.com>
2016-06-24 12:13:12 +0200

    Add tag-based searching for tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -14,6 +14,10 @@ class Ticket < ActiveRecord::Base
 
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
 
+  searcher do
+    label :tag, from: :tags, field: "name"
+  end
+
   before_create :assign_default_state
 
   def tag_names=(names)
